0a2b87434e2c342f6afdecd699a45310dee1098e
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 03:53:46 +0700

    mac menu

---
+++ MacMenu/MacMenu/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacMenu
+//
+//  Created by Adrian Suryo Abiyoga on 02/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+        VStack{
+            Text("macOS Menu App")
+        }
+        .frame(width: 300, height: 300)
+    }
+}
+
+#Preview {
+    ContentView()
+}
